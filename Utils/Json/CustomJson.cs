3070d5f2b760919b06cc54122042ee634c73e150
Hoang Tien Minh <htminh.1001@gmail.com>
2023-05-30 23:56:10 +0700

    feat: add connection service to MongoDB

---
+++ Utils/Json/CustomJson.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace Utils.Json
+{
+    public class CustomJson
+    {
+        public static string Stringify<T>(T input)
+        {
+            return JsonSerializer.Serialize<T>(input);
+        }
+
+        public static T? Deserialzie<T>(string input)
+        {
+            return JsonSerializer.Deserialize<T>(input);
+        }
+    }
+}
+
---
+++ novaan-server/Developer/DevController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using MongoConnector;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NovaanServer.Developer
+{
+	[Route("api/dev")]
+	[ApiController]
+	public class DeveloperController: Controller
+	{
+		private readonly MongoDBService _mongoService;
+
+		public DeveloperController(MongoDBService mongoDBService)
+		{
+			_mongoService = mongoDBService;
+		}
+
+        [HttpGet("health")]
+        public IActionResult GetHealthCheck()
+        {
+            return Ok("Server is up and running");
+        }
+
+        [HttpGet("database-health")]
+		public IActionResult GetDatabaseHealthCheck()
+		{
+			if(_mongoService.PingDatabase())
+			{
+                return Ok("Database is up and running");
+            }
+
+			return BadRequest();
+        }
+	}
+}
+
