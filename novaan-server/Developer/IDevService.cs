8533ab63f995f8b7ceaf63508f78a76ed5a61641
Hoang Tien Minh <htminh.1001@gmail.com>
2023-06-01 00:49:14 +0700

    chore: add test project and add test demo. Add README on what to test and how to test. Update Github Actions CI

---
+++ novaan-server/Developer/IDevService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace NovaanServer.Developer
+{
+	public interface IDevService
+	{
+		public bool IsServerHealthy();
+
+		public bool IsDatabaseHealthy();
+
+		public bool IsS3Healthy();
+	}
+}
+
