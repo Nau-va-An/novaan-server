1089927cea8215de08179c1b3f6909ace88b266f
dukentt <ducnthe153062@fpt.edu.vn>
2023-06-07 14:29:47 +0700

    Add Basic Authen feature, Adding Jwt service include generate token and refresh token

---
+++ novaan-server/Configuration/JwtConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace NovaanServer.Configuration
+{
+    public class JwtConfig
+    {
+        public string Secret { get; set; }
+
+        public TimeSpan ExpiryTimeFrame { get; set; }
+    }
+}
--- novaan-server/src/Auth/AuthService.cs
+++ novaan-server/src/Auth/AuthService.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
 using MongoConnector;
 using MongoConnector.Models;
 using MongoDB.Bson;
@@ -18,9 +21,26 @@ namespace NovaanServer.Auth
             _mongoService = mongoDBService;
         }
 
-        public Task<bool> SignInWithCredentials(SignInDTOs signInDTO)
+        public async Task<bool> SignInWithCredentials(SignInDTOs signInDTO)
         {
-            throw new NotImplementedException();
+            var foundUser = _mongoService.Accounts
+                 .Find(
+                acc => acc.Email == signInDTO.UsernameOrEmail
+                )
+                 .FirstOrDefault();
+
+            if(foundUser == null)
+            {
+               throw new BadHttpRequestException(ExceptionMessage.EMAIL_OR_PASSWORD_NOTFOUND);
+            }
+
+            var hashPassword = CustomHash.GetHashString(signInDTO.Password);
+            if (foundUser.Password != hashPassword)
+            {
+                throw new BadHttpRequestException(ExceptionMessage.EMAIL_OR_PASSWORD_NOTFOUND);
+            }
+
+            return true;
         }
 
         public Task<bool> SignInGoogle()
@@ -111,6 +131,7 @@ namespace NovaanServer.Auth
                 .FirstOrDefaultAsync();
             return foundAccount != null;
         }
+
     }
 }
 
