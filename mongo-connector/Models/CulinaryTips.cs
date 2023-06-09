cb382b2b28bb1b5dbfab652cad4240884e87c512
meof-coding <tranthe150186@fpt.edu.vn>
2023-06-10 02:50:32 +0700

    feat: Add Culinary Tips model

---
+++ mongo-connector/Models/CulinaryTips.cs
@@ -0,0 +1,15 @@
+namespace MongoConnector.Models
+{
+    public class CulinaryTips
+    {
+        // ContentID
+        public string? Id { get; set; }
+        // Title
+        public string? Title { get; set; }
+        // VideoURL
+        public string? VideoURL { get; set; }
+        // AccountID
+        public string? AccountID { get; set; }
+    }
+    
+}
