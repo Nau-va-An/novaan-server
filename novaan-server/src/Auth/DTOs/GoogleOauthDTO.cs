d762b731080bd4ade605c27e24a0f825e7bb8e16
meof-coding <tranthe150186@fpt.edu.vn>
2023-06-12 20:27:29 +0700

    refactor: Set all properties in GoogleOauthDTO as required
    Updated the GoogleOauthDTO class to set all properties as required and removed the nullable annotations.
    
    Changes made in this commit:
    
    - Set all properties in the GoogleOauthDTO class as required.
    - Removed the nullable annotations for each property.
    
    This commit enforces that all properties in the GoogleOauthDTO class must be provided with non-null values, ensuring the integrity and validity of the data.

--- novaan-server/src/Auth/DTOs/GoogleOauthDTO.cs
+++ novaan-server/src/Auth/DTOs/GoogleOauthDTO.cs
@@ -1,14 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace NovaanServer.src.Auth.DTOs
 {
 	public class GoogleOauthDTO
 	{
+		[Required]
 		// Username
-		public string? Name { get; set; }
+		public string Name { get; set; }
+		[Required]
 		// Email
-		public string? Email { get; set; }
+		public string Email { get; set; }
+		[Required]
 		// Google ID
-		public string? Sub { get; set; }
+		public string Sub { get; set; }
 	}
 }
 
