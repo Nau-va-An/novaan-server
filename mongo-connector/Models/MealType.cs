8a1b257f6f7efd07944de71cda42f19dda6a493c
Hoàng Tiến Minh <htminh.1001@gmail.com>
2023-06-18 13:13:42 +0700

    Hotfix: Update missing GoogleOAuthDTO file due to git cache issue (#25)
    * fix: update oauth with google to get info from google server
    
    * chore: cleanup AuthService
    
    * fix: update error response format
    
    * fix: fix not checking email
    
    * refactor: separate get google account login to another function
    
    * fix: update missing add User profile when sign up
    
    * fix: fix commented issues
    
    * chore: update missing GoogleOAuthDTO files
    
    * fix: fix model files format

--- mongo-connector/Models/MealType.cs
+++ mongo-connector/Models/MealType.cs
@@ -9,7 +9,8 @@ namespace MongoConnector.Models
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
-        public string Title { get; set; }
+
+        public string Title { get; set; } = string.Empty;
     }
 }
 
