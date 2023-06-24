0aad53278c05273518d69e254aeaf4e191aede4d
meof-coding <tranthe150186@fpt.edu.vn>
2023-06-24 15:35:10 +0700

    fix mapping from request body to server object and add requried validation for dto

--- novaan-server/src/Content/DTOs/FileInformationDTO.cs
+++ novaan-server/src/Content/DTOs/FileInformationDTO.cs
@@ -1,11 +1,22 @@
-﻿namespace NovaanServer.src.Content.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NovaanServer.src.Content.DTOs
 {
     public class FileInformationDTO
     {
+        [Required]
         public long FileSize { get; set; }
+        
+        [Required]
         public int Width { get; set; }
+        
+        [Required]
         public int Height { get; set; }
+        
+        [Required]
         public string? FileExtension { get; set; }
+        
+        [Required]
         public TimeSpan VideoDuration { get; set; }
     }
 
