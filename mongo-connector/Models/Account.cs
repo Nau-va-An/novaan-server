ca08ef359bd4ae9bab53ea217db2eadcd576bcc0
meof-coding <tranthe150186@fpt.edu.vn>
2023-06-25 10:39:29 +0700

    add userId reference to account collection and remove Account reference in User collection

--- mongo-connector/Models/Account.cs
+++ mongo-connector/Models/Account.cs
@@ -9,8 +9,9 @@ namespace MongoConnector.Models
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
 
-        public string DisplayName { get; set; }
-
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string UserId { get; set; }
+        
         public string Email { get; set; }
 
         public string Password { get; set; }
