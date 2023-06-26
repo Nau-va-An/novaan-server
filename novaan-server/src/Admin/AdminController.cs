12af6654a6d5f244f94d3f5a7883d4f0369d54c4
Hoang Tien Minh <htminh.1001@gmail.com>
2023-06-26 16:18:08 +0700

    fix: recipes and culinary tips not backward compatible if there are new field(s)

--- novaan-server/src/Admin/AdminController.cs
+++ novaan-server/src/Admin/AdminController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MongoConnector.Enums;
 using MongoConnector.Models;
