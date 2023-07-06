346b7f296314f54d15727b809d1d640db7e78b29
meof-coding <tranthe150186@fpt.edu.vn>
2023-07-06 13:52:52 +0700

    fix: add endedArray variable

--- novaan-server/src/Search/SearchService.cs
+++ novaan-server/src/Search/SearchService.cs
@@ -20,64 +20,62 @@ namespace NovaanServer.src.Search
         {
             // read basic ingredients from the json file in same folder
             var basicIngredients = File.ReadAllText("BasicIngredients.json");
-            var listBasicIngredients = JsonConvert
-                .DeserializeObject<List<string>>(basicIngredients)
-                ?? new List<string>();
+            var listBasicIngredients = JsonConvert.DeserializeObject<List<string>>(basicIngredients) ?? new List<string>();
 
-            //Remove the basic ingredients ignore case from the input list, resulting in a list of ingredients without the basic ones.
+            // Remove the basic ingredients, ignoring case, from the input list
             var ingredients = req.Ingredients
                 .Where(i => !listBasicIngredients.Contains(i, StringComparer.OrdinalIgnoreCase))
                 .ToList();
 
-            Dictionary<string, List<int>> recipeMappings = new();
+            var recipeMappings = new Dictionary<string, List<int>>();
 
+            // Initialize counters and variables
             int i = 0;
+            int endedArray = 0;
 
-            while (true)
+            while (endedArray < ingredients.Count)
             {
                 if (i >= ingredients.Count)
                 {
                     i = 0;
+                    endedArray++;
                     continue;
-                }
-
-                if (recipeMappings.Count >= ingredients.Count)
-                {
-                    break;
                 }
 
                 var ingredient = ingredients[i];
-                var ingredientToRecipes = (await _mongoDBService.IngredientToRecipes
-                    .FindAsync(i => i.Ingredient == ingredient))
+                var ingredientToRecipes =  (await _mongoDBService.IngredientToRecipes
+                    .Find(i => i.Ingredient.Equals(ingredient, StringComparison.OrdinalIgnoreCase))
+                    .ToListAsync())
                     .FirstOrDefault();
 
-                if (ingredientToRecipes == null){
-                    continue;
-                }
-
-                foreach (var recipeId in ingredientToRecipes.RecipeIds)
+                // Iterate over the recipe IDs associated with the current ingredient and update the recipeMappings dictionary accordingly. 
+                // If the recipe ID already exists in the dictionary, append the current ingredient index to the corresponding list.
+                // Otherwise, create a new entry in the dictionary with the recipe ID as the key and a list containing the current ingredient index.
+                if (ingredientToRecipes != null)
                 {
-                    if (recipeMappings.ContainsKey(recipeId))
+                    foreach (var recipeId in ingredientToRecipes.RecipeIds)
                     {
-                        recipeMappings[recipeId].Add(i);
-                    }
-                    else
-                    {
-                        recipeMappings.Add(recipeId, new List<int> { i });
+                        if (recipeMappings.ContainsKey(recipeId))
+                        {
+                            recipeMappings[recipeId].Add(i);
+                        }
+                        else
+                        {
+                            recipeMappings.Add(recipeId, new List<int> { i });
+                        }
                     }
                 }
 
                 i++;
             }
 
-            // After the loop ends, extract the recipe arrays from the dictionary.
+            // Extract the recipe IDs
             var recipeIds = recipeMappings
                 .Where(r => r.Value.Count == ingredients.Count)
                 .Select(r => r.Key)
-                .ToList()
-                ?? throw new NovaanException(ErrorCodes.SEARCH_INGREDIENTS_NOT_FOUND, HttpStatusCode.NotFound);
+                .ToList();
 
-            //Return the extracted arrays, which represent the recipes that can be made using the input ingredients.
+            // Return the recipes that can be made using the input ingredients
             return await _mongoDBService.Recipes
                 .Find(r => recipeIds.Contains(r.Id))
                 .Project(r => new GetRecipesByIngredientsRes
@@ -88,6 +86,7 @@ namespace NovaanServer.src.Search
                     CookTime = r.CookTime,
                 })
                 .ToListAsync();
+
         }
     }
 }
